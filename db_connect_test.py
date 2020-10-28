bcb530456f6b4d9c9c12298cd7ce48edf810a380
Shim Jung Hyun <shj030220@gmail.com>
2020-10-28 16:14:12 +0900

    [Test] MySQL DB Connect Test

--- db_connect_test.py
+++ db_connect_test.py
@@ -1,9 +1,16 @@
 import pymysql
+import pandas as pd
 from db_connect_setting import DB
 
 cursor = DB.cursor(pymysql.cursors.DictCursor)
-sql = "select * from user where email='a@gmail.com';"
+sql = "SELECT *" \
+      "FROM user_skillset;"
 
 cursor.execute(sql)
 result = cursor.fetchall()
 print(result)
+
+result = pd.DataFrame(result)
+print(result)
+
+DB.close()

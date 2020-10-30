95992490b744b1f9657641e0c9132d0df9a15a4b
Shim Jung Hyun <shj030220@gmail.com>
2020-10-31 02:47:07 +0900

    [Test] Select Query access automation test

---
+++ DB_Automation_Test.py
@@ -0,0 +1,28 @@
+import pymysql
+import pandas as pd
+from db_connect_setting import DB
+
+"""
+TEST SQL Query
+select user_skillset.user_email, user_skillset.skill, user_search_log.log, user_view_log.log
+from user_skillset, user_search_log, user_view_log
+where user_skillset.user_email='a@gmail.com' and user_skillset.user_email=user_search_log.user_email and user_skillset.user_email=user_view_log.user_email;
+"""
+
+def _sql_set(email):
+    sql = "select user_skillset.user_email, user_skillset.skill, user_search_log.log, user_view_log.log from user_skillset, user_search_log, user_view_log where user_skillset.user_email="\
+          + "'" + str(email) + "' " + "and user_skillset.user_email=user_search_log.user_email and user_skillset.user_email=user_view_log.user_email;"
+
+    return sql
+
+cursor = DB.cursor(pymysql.cursors.DictCursor)
+cursor.execute(_sql_set("a@gmail.com"))
+result = cursor.fetchall()
+
+for i in result:
+    print(i)
+
+pd_result = pd.DataFrame(result)
+print(result)
+
+DB.close()

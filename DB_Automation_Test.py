ffebdeb7e1b5c3c18966245a4cc4245311bb985d
Shim Jung Hyun <shj030220@gmail.com>
2020-11-04 15:35:28 +0900

    [Style] Add Comment

--- DB_Automation_Test.py
+++ DB_Automation_Test.py
@@ -4,22 +4,20 @@ import numpy as np
 from db_connect_setting import DB
 
 """
-TEST SQL Query
-select user_skillset.user_email, user_skillset.skill, user_search_log.log, user_view_log.log
-from user_skillset, user_search_log, user_view_log
-where user_skillset.user_email='a@gmail.com' and user_skillset.user_email=user_search_log.user_email and user_skillset.user_email=user_view_log.user_email;
-"""
+==========Get user skillset==========
+select user.email as 'email', user_skillset.skill as 'user_skill'
+from user, user_skillset
+where user.email='a@gmail.com' and user.email=user_skillset.user_email;
 
-"""
-TEST SQL Query2
-select user.email as 'email', user_skillset.skill as 'user_skillset', 
-user_search_log.log as 'user_search_log', user_view_log.log as 'user_view_log'
-from user, user_skillset, user_search_log, user_view_log;
-"""
+==========Get user search log==========
+select user.email as 'email', user_search_log.log as 'search_log'
+from user, user_search_log
+where user.email='a@gmail.com' and user.email=user_search_log.user_email;
 
-"""
-Need Data
-UserSkillSets, UserSearchLogs, UserViewLogs, UserViewPostID, UserViewPostTitle, UserViewPostContent
+==========Get user view log==========
+select user.email as 'email', user_view_log.log as 'view_log'
+from user, user_view_log
+where user.email='a@gmail.com' and user.email=user_view_log.user_email;
 """
 
 def _sql_set(email):

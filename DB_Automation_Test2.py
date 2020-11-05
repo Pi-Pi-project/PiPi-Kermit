ee8015bb9f3186dc45d7c84c770b8f79e2e7dbde
Shim Jung Hyun <shj030220@gmail.com>
2020-11-05 15:10:29 +0900

    [Fix] Fix Issue

--- DB_Automation_Test2.py
+++ DB_Automation_Test2.py
@@ -7,31 +7,32 @@ email = "a@gmail.com"
 
 def  _user_skill(email):
     sql_query = "select user.email as 'email', user_skillset.skill as 'user_skill' from user, user_skillset where user.email=" \
-                + "'" + str(email) + "' " + "and user.email=user_skillset.user_email;"
+                + "'" + str(email) + "' " + "and user.email=user_skillset.user_email"
 
     return sql_query
 
 def _user_search_log(email):
-    sql_query = "select user.email as 'email', user_search_log.log as 'search_log' from user, user_search_log" \
-                + "'" + str(email) + "' " + "and user.email=user_search_log.user_email;"
+    sql_query = "select user.email as 'email', user_search_log.log as 'search_log' from user, user_search_log where user.email=" \
+                + "'" + str(email) + "' " + "and user.email=user_search_log.user_email"
 
     return sql_query
 
 def _user_view_log(email):
-    sql_query = "select user.email as 'email', post.id as 'post_id', post.title as 'title', post.content as 'post_content', user_view_log.log as 'view_log'" \
-                + "'" + str(email) + "' " + "and user.email=user_view_log.user_email;"
+    sql_query = "select user.email as 'email', post.id as 'post_id', post.title as 'title', post.content as 'post_content', user_view_log.log as 'view_log' from user, post, user_view_log where user.email=" \
+                + "'" + str(email) + "' " + "and user.email=user_view_log.user_email"
 
     return sql_query
 
-cursor = DB.cursor(pymysql.cursors.DictCursor)
-
+cursor = DB.cursor()
 cursor.execute(_user_skill(email))
-skill_result = cursor.fetchall()
-
-cursor.execute(_user_search_log(email))
-search_result = cursor.fetchall()
+result1 = pd.DataFrame(cursor.fetchall())
 
 cursor.execute(_user_view_log(email))
-view_result = cursor.fetchall()
+result2 = pd.DataFrame(cursor.fetchall())
 
-DB.close()
+cursor.execute(_user_search_log(email))
+result3 = pd.DataFrame(cursor.fetchall())
+
+print(result1)
+print(result2)
+print(result3)

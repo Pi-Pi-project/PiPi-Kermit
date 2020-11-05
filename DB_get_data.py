ec214be576a53cf9752afb3a1321b911a13f27a5
Shim Jung Hyun <shj030220@gmail.com>
2020-11-05 16:50:36 +0900

    [Feat] Add function get data

---
+++ DB_get_data.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import pymysql
+from DB_connect_setting import DB
+
+def  _user_skill(email):
+    sql_query = "select user.email as 'email', user_skillset.skill as 'user_skill' from user, user_skillset where user.email=" \
+                + "'" + str(email) + "' " + "and user.email=user_skillset.user_email"
+
+    return sql_query
+
+def _user_search_log(email):
+    sql_query = "select user.email as 'email', user_search_log.log as 'search_log' from user, user_search_log where user.email=" \
+                + "'" + str(email) + "' " + "and user.email=user_search_log.user_email"
+
+    return sql_query
+
+def _user_view_log(email):
+    sql_query = "select user.email as 'email', post.id as 'post_id', post.title as 'title', post.content as 'post_content', user_view_log.log as 'view_log' from user, post, user_view_log where user.email=" \
+                + "'" + str(email) + "' " + "and user.email=user_view_log.user_email"
+
+    return sql_query
+
+email = "a@gmail.com"
+cursor = DB.cursor(pymysql.cursors.DictCursor)
+
+try:
+    cursor.execute(_user_skill(email))
+    user_skill = pd.DataFrame(cursor.fetchall())
+
+    cursor.execute(_user_search_log(email))
+    user_search_log = pd.DataFrame(cursor.fetchall())
+
+    cursor.execute(_user_view_log(email))
+    user_view_log = pd.DataFrame(cursor.fetchall())
+
+finally:
+    DB.close()
+
+# Test
+print(user_skill)
+print(user_search_log)
+print(user_view_log)

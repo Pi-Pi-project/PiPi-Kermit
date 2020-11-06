3f6ccf6a0d4ca0aad591a4b32bee4436993a7b48
Shim Jung Hyun <shj030220@gmail.com>
2020-11-06 19:10:05 +0900

    [Test] View Dataframe information

--- DB_preprocessing.py
+++ DB_preprocessing.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from DB_connect_setting import DB
-from DB_get_data import _user_skill, _user_search_log, _user_view_log
+from DB_get_data import _user_skill, _user_search_log, _user_view_log, df_information
 
-email = ""
+email = "a@gmail.com"
 
 try:
     user_skill = _user_skill(email)
@@ -11,3 +11,7 @@ try:
 
 finally:
     DB.close()
+
+df_information(user_skill)
+df_information(user_search_log)
+df_information(user_view_log)

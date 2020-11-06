930cbca6c530dd8e2259a71fe594c1a537a2e71c
Shim Jung Hyun <shj030220@gmail.com>
2020-11-06 20:23:28 +0900

    [Feat] Slicing data and converting DataFrame to list

--- DB_preprocessing.py
+++ DB_preprocessing.py
@@ -5,13 +5,25 @@ from DB_get_data import _user_skill, _user_search_log, _user_view_log, df_inform
 email = "a@gmail.com"
 
 try:
-    user_skill = _user_skill(email)
-    user_search_log = _user_search_log(email)
-    user_view_log = _user_view_log(email)
+    user_skill_df = _user_skill(email)
+    user_search_log_df = _user_search_log(email)
+    user_view_log_df = _user_view_log(email)
 
 finally:
     DB.close()
 
-df_information(user_skill)
-df_information(user_search_log)
-df_information(user_view_log)
+# df_information(user_skill_df)
+# df_information(user_search_log_df)
+# df_information(user_view_log_df)
+
+user_skill_list = list(user_skill_df["user_skill"])
+user_search_log_list = list(user_search_log_df["search_log"])
+user_view_log_loc = user_view_log_df.loc[:, ["post_id", "title", "post_content", "view_log"]]
+
+print(user_view_log_loc)
+
+user_view_log_list = []
+print(list(user_view_log_loc.loc[0]))
+
+# Get row length
+print(len(user_view_log_loc))

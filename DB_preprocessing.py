18eb33b05ac2ae77125a674082461bd5876c321f
Shim Jung Hyun <shj030220@gmail.com>
2020-11-06 15:16:59 +0900

    [Feat] Preprocess Data Recall

--- DB_preprocessing.py
+++ DB_preprocessing.py
@@ -1,3 +1,13 @@
 import pandas as pd
-from DB_get_data import user_skill, user_search_log, user_view_log
+from DB_connect_setting import DB
+from DB_get_data import _user_skill, _user_search_log, _user_view_log
 
+email = ""
+
+try:
+    user_skill = _user_skill(email)
+    user_search_log = _user_search_log(email)
+    user_view_log = _user_view_log(email)
+
+finally:
+    DB.close()

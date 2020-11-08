0f2763f83ac0c3d521843f0a9304c97ed0a3f7be
Shim Jung Hyun <shj030220@gmail.com>
2020-11-08 20:25:02 +0900

    [Refactor] Refactor list convert part

--- DB_preprocessing.py
+++ DB_preprocessing.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from DB_connect_setting import DB
 from DB_get_data import _user_skill, _user_search_log, _user_view_log
-from konlpy.tag import Okt
+from eunjeon import Mecab
 
-okt = Okt()
+mecab = Mecab()
 email = "a@gmail.com"
 
 try:
@@ -13,6 +13,7 @@ try:
     view_log_df = _user_view_log(email)
 
 finally:
+    # Close the connection from the MySQL DB
     DB.close()
 
 search_log_len = len(search_log_df)
@@ -20,16 +21,16 @@ view_log_len = len(view_log_df)
 
 skill_list = list(skill_df["user_skill"])
 
-search_log_list = list(search_log_df["search_log"].iloc[:search_log_len-6:-1])
+search_log_list = list(search_log_df["search_log"].iloc[search_log_len-6::1])
 search_log_list.reverse()
 
-view_title_list = list(view_log_df["title"].iloc[:view_log_len-6:-1])
+view_title_list = list(view_log_df["title"].iloc[view_log_len-6::1])
 view_title_list.reverse()
 
-view_content_list = list(view_log_df["post_content"].iloc[:view_log_len-6:-1])
+view_content_list = list(view_log_df["post_content"].iloc[view_log_len-6::1])
 view_content_list.reverse()
 
-view_log_list = list(view_log_df["view_log"].iloc[:view_log_len-6:-1])
+view_log_list = list(view_log_df["view_log"].iloc[view_log_len-6::1])
 view_log_list.reverse()
 
 # Test
@@ -39,5 +40,5 @@ print(view_title_list)
 print(view_content_list)
 print(view_log_list)
 
-print(okt.morphs(view_content_list[0]))
-print(okt.nouns(view_content_list[0]))
+# print(mecab.morphs(view_content_list[0]))
+# print(mecab.nouns(view_content_list[0]))

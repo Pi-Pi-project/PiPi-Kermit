385eaab4d9e708ecfe8e399ee0067c1e1f2eb914
Shim Jung Hyun <shj030220@gmail.com>
2020-11-13 14:56:25 +0900

    [Feat] Add morphs_stopword

--- DB_preprocessing.py
+++ DB_preprocessing.py
@@ -7,16 +7,29 @@ from eunjeon import Mecab
 email = "a@gmail.com"
 
 # 특수문자 제거를 위한 정규 표현식
-def regex(text_val):
+def regex(texts):
     result = []
-    for val in text_val:
-        result.append(re.sub('[!@#$%^&*()_+~`]', '', val))
+
+    for text in texts:
+        result.append(re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣0-9a-zA-Z ]', '', text))
 
     return result
 
-def token_data(email):
+def morphs_stopword(texts):
+    STOP_WORDS = ['의', '가', '이', '은', '들', '는', '좀', '잘', '걍', '과',
+                  '도', '를', '으로', '자', '에', '와', '한', '하다']
     mecab = Mecab()
+    result = []
 
+    for text in texts:
+        tmp = mecab.morphs(text)
+
+        if tmp not in STOP_WORDS:
+            result.append(tmp)
+
+    return result
+
+def processed_data(email):
     try:
         # Get Data from MySQL DB
         skill_df = _user_skill(email)
@@ -34,9 +47,10 @@ def token_data(email):
     # 최신의 로그 5개를 추출
     skill_list = list(skill_df["user_skill"])
     search_log_list = list(search_log_df["search_log"].iloc[search_log_len - 6::1])
-    view_title_list = regex(list(view_log_df["title"].iloc[view_log_len - 6::1]))
-    view_content_list = regex(list(view_log_df["post_content"].iloc[view_log_len - 6::1]))
-    view_log_list = regex(list(view_log_df["view_log"].iloc[view_log_len - 6::1]))
+    view_log_list = list(view_log_df["view_log"].iloc[view_log_len - 6::1])
+    # 분석 시 오류를 줄이고 정확도를 늘리기 위해 추가적인 전처리 진행
+    view_title_list = morphs_stopword(regex(view_log_df["title"].iloc[view_log_len - 6::1]))
+    view_content_list = morphs_stopword(regex(view_log_df["post_content"].iloc[view_log_len - 6::1]))
 
     # 최신순을 기준으로 오름차순 정렬하기 위해서 reverse 적용
     search_log_list.reverse()
@@ -44,11 +58,13 @@ def token_data(email):
     view_content_list.reverse()
     view_log_list.reverse()
 
-    return search_log_list, view_title_list, view_content_list, view_log_list
+    return skill_list, search_log_list, view_title_list, view_content_list, view_log_list
 
-# Test
-a, b, c, d = token_data(email)
-print(a)
-print(b)
-print(c)
-print(d)
+# Test Output
+print("========================[Test Output]========================")
+SL, SLL, VTL, VCL, VLL = processed_data(email)
+print("[SL]\n", SL, "\n")
+print("[SLL]\n", SLL, "\n")
+print("[VTL]\n", VTL, "\n")
+print("[VCL]\n", VCL, "\n")
+print("[VLL]\n", VLL, "\n")

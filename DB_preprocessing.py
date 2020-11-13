8a2019d4ecfc952441e6adf09784294955a73ed8
Shim Jung Hyun <shj030220@gmail.com>
2020-11-13 13:41:11 +0900

    [Feat] Change some code to the form of a function

--- DB_preprocessing.py
+++ DB_preprocessing.py
@@ -3,21 +3,8 @@ from DB_connect_setting import DB
 from DB_get_data import _user_skill, _user_search_log, _user_view_log
 from eunjeon import Mecab
 
-
 # Test Email
 email = "a@gmail.com"
-
-mecab = Mecab()
-
-try:
-    # Get Data from MySQL DB
-    skill_df = _user_skill(email)
-    search_log_df = _user_search_log(email)
-    view_log_df = _user_view_log(email)
-
-finally:
-    # Close the connection from the MySQL DB
-    DB.close()
 
 # 특수문자 제거를 위한 정규 표현식
 def regex(text_val):
@@ -27,26 +14,41 @@ def regex(text_val):
 
     return result
 
-search_log_len = len(search_log_df)
-view_log_len = len(view_log_df)
+def token_data(email):
+    mecab = Mecab()
 
-# Convert DataFrame to list
-# 최신의 로그 5개를 추출
-skill_list = list(skill_df["user_skill"])
-search_log_list = list(search_log_df["search_log"].iloc[search_log_len-6::1])
-view_title_list = regex(list(view_log_df["title"].iloc[view_log_len-6::1]))
-view_content_list = regex(list(view_log_df["post_content"].iloc[view_log_len-6::1]))
-view_log_list = regex(list(view_log_df["view_log"].iloc[view_log_len-6::1]))
+    try:
+        # Get Data from MySQL DB
+        skill_df = _user_skill(email)
+        search_log_df = _user_search_log(email)
+        view_log_df = _user_view_log(email)
 
-# 최신순을 기준으로 오름차순 정렬하기 위해서 reverse 적용
-search_log_list.reverse()
-view_title_list.reverse()
-view_content_list.reverse()
-view_log_list.reverse()
+    finally:
+        # Close the connection from the MySQL DB
+        DB.close()
+
+    search_log_len = len(search_log_df)
+    view_log_len = len(view_log_df)
+
+    # Convert DataFrame to list
+    # 최신의 로그 5개를 추출
+    skill_list = list(skill_df["user_skill"])
+    search_log_list = list(search_log_df["search_log"].iloc[search_log_len - 6::1])
+    view_title_list = regex(list(view_log_df["title"].iloc[view_log_len - 6::1]))
+    view_content_list = regex(list(view_log_df["post_content"].iloc[view_log_len - 6::1]))
+    view_log_list = regex(list(view_log_df["view_log"].iloc[view_log_len - 6::1]))
+
+    # 최신순을 기준으로 오름차순 정렬하기 위해서 reverse 적용
+    search_log_list.reverse()
+    view_title_list.reverse()
+    view_content_list.reverse()
+    view_log_list.reverse()
+
+    return search_log_list, view_title_list, view_content_list, view_log_list
 
 # Test
-print(skill_list)
-print(search_log_list)
-print(view_title_list)
-print(view_content_list)
-print(view_log_list)
+a, b, c, d = token_data(email)
+print(a)
+print(b)
+print(c)
+print(d)

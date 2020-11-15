288b9cf60528360538f7d18399d9dff8a80f990f
Shim Jung Hyun <shj030220@gmail.com>
2020-11-15 18:33:48 +0900

    [Test] Tokenizer Test

--- Keras_Tokenizer_Test.py
+++ Keras_Tokenizer_Test.py
@@ -1,3 +1,15 @@
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 tokenizer = Tokenizer()
+
+samples = ["The cat sat on the mat.", "The dog ate my homework"]
+
+tokenizer.fit_on_texts(samples)
+
+sequences = tokenizer.texts_to_sequences(samples)
+
+# Mode: binary, count, tfidf, freq
+one_hot_result = tokenizer.texts_to_matrix(samples, mode="binary")
+
+word_index = tokenizer.word_index
+print("Found %s unique tokens" % len(word_index))

16c8bb9a2500c955ed0265ccffee0c3a82fb8348
Shim Jung Hyun <shj030220@gmail.com>
2020-11-21 23:03:58 +0900

    [Test] mecab, re Test

--- Embedding_Tokenization_Test.py
+++ Embedding_Tokenization_Test.py
@@ -2,4 +2,23 @@ import re
 from eunjeon import Mecab
 mecab = Mecab()
 
-sentence = "앨릭스 에이자 미국 보건복지부(HHS) 장관은 20일(현지시간) 조 바이든 당선인이 ‘신종 코로나바이러스 감염증(코로나19) 대응 정보 공유 지연으로 인명 피해가 발생할 수 있다’고 경고한 것에 대해 “인수인계 지연으로 생명을 잃게 될 수 있다는 바이든씨(Mr.Biden)는 완전히 틀렸다(absolutely incorrect)”고 했다. 에이자 장관은 이날 CBS 시사 프로그램 ‘디스 이즈 모닝’과 인터뷰에서 “백신 보급 계획을 포함해 도널드 트럼프 행정부가 하는 모든 조치는 매우 투명하다”며 이같이 밝혔다.에이자 장관은 “비밀 자료는 없다”고도 단언했다. 바이든 당선인은 코로나19 대응 정보에 접근하지 못하면 대응 노력을 저해하고 인명 피해를 유발할 수 있다고 비난한 바 있다. 한편, 바이든 당선인의 코로나19 자문위원인 애툴 가완데 하버드 의대 교수는 이날 같은 프로그램에 출연해 “유례 없는 (코로나19) 백신 보급에 제대로 대비하기 위해서는 더 많은 정보가 필요하다”고 했다."
+sentence = "20일 0시 기준 코로나19 신규 확진자는 363명. 사흘 연속 300명을 넘었다. 서울 경기 등 수도권 확산세가 걷잡을 수 없다. 21일 중등교사임용시험을 앞두고 이날 서울 동작구 노량진의 한 대형 학원과 관련해 최소 39명의 확진자가 나왔다. 임용시험 응시자는 전국적으로 6만 명이 넘는다. 2주도 남지 않은 대학수학능력시험 방역에 대한 불안감도 커지고 있다."
+
+# 1. 표제어 추출(Lemmatization)
+print("품사 태깅 - PoS\n" + str(mecab.pos(sentence)) + "\n")
+print("품사 태깅 - Morphs\n" + str(mecab.morphs(sentence)) + "\n")
+print("품사 태깅 - Nouns\n" + str(mecab.nouns(sentence)) + "\n")
+print("======================================================")
+
+# 2. 불용어 제거(Stopwords Removing)
+def text_preprocessing(text, tokenizer):
+    STOPWORDS = ['을', '를', '이', '가', '은', '는']
+
+    txt = re.sub("[^가-힣a-z]", " ", text)
+    token = tokenizer.morphs(txt)
+    token = [t for t in token if t not in STOPWORDS]
+
+    return token
+
+example = text_preprocessing(sentence, mecab)
+print("불용어 제거\n" + str(example))
